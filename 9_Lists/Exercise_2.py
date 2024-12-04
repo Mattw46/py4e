64e521de6857a7b8f9e45a11f78c9e627fd30270
Matt <mattw76@live.com.au>
2024-12-05 10:47:14 +1100

    Minor Update
    remove debug

--- 9_Lists/Exercise_2.py
+++ 9_Lists/Exercise_2.py
@@ -1,9 +1,8 @@
 fhand = open('exercise2.txt')
-#fhand = open('mbox-short.txt')
+
 count = 0
 for line in fhand:
     words = line.split()
-    # print('Debug:', words)
     if len(words) < 3 : continue
     if words[0] != 'From' : continue
     print(words[2])
--- 9_Lists/Exercise_3.py
+++ 9_Lists/Exercise_3.py
@@ -1,8 +1,6 @@
 fhand = open('exercise2.txt')
-#fhand = open('mbox-short.txt')
 count = 0
 for line in fhand:
     words = line.split()
-    # print('Debug:', words)
     if len(words) < 3 or words[0] != 'From' : continue
     print(words[2])
